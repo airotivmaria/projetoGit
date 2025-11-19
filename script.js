812338944659dc9ef2347fd84a0477b5c1aec8b1
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2025-11-19 17:40:04 -0300

    ✨ feat: Melhorias na UX e atualização da documentação
    * :art: feat: modifica a cor do body
    
    * :memo: docs (scrip.js): adicionado comentário
    
    * ✨ feat: Adiciona formatação em negrito no valor do resultado
    
    * 📝 docs: Adiciona seção de instruções de uso no README
    
    ---------
    
    Co-authored-by: Maria Vitória <153694001+airotivmaria@users.noreply.github.com>

--- script.js
+++ script.js
@@ -30,5 +30,6 @@ const fatores = {
         const resultadoFormatado = resultado.toLocaleString('pt-BR', { maximumFractionDigits: 4 });
 
         // mensagem do resultado
+        resultadoDiv.innerHTML = `✅ ${valor} ${unidadeDe} equivalem a **${resultadoFormatado}** ${unidadePara} ✅`;
         resultadoDiv.innerHTML = `✅ ${valor} ${unidadeDe} equivalem a ${resultadoFormatado} ${unidadePara} ✅`;
     }
